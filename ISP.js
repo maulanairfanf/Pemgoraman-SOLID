f24ba924c3c95b7e22e282f5db07933974cea6f2
maulanairfanf <maulanairfanf@gmail.com>
2020-10-02 00:18:46 +0700

    stak

--- ISP.js
+++ ISP.js
@@ -1,27 +1,56 @@
-class Gopay extends eWalletProviderMixins(PaymentProvider) {
-    paymentName() { }
-    amount() { }
-    accountID() { }
-    walletProviderID() { }
- }
-  
- class Mandiri extends bankProviderMixins(PaymentProvider) {
-    paymentName() { }
-    amount() { }
-    bankID() { }
-    virtualAccount() { }
- }
-  
- class BNI extends bankProviderMixins(PaymentProvider) {
-    paymentName() { }
-    amount() { }
-    bankID() { }
-    virtualAccount() { }
- }
+let eWalletProviderMixins = Base => class extends Base {
+   accountID() {}
+   walletProviderID() {}
+};
 
- class OVO extends eWalletProviderMixins(PaymentProvider) {
-    paymentName() { }
-    amount() { }
-    accountID() { }
-    walletProviderID() { }
- }
+let bankProviderMixins = Base => class extends Base {
+   bankID() {
+      console.log("bank id")
+   }
+   virtualAccount() {}
+};
+
+class PaymentProvider {
+   paymentName() {
+      console.log("payment provider")
+   }
+   amount() {
+      console.log("amount")
+   }
+}
+
+class Gopay extends eWalletProviderMixins(PaymentProvider) {
+   paymentName() {}
+   amount() {}
+   accountID() {}
+   walletProviderID() {}
+}
+
+class Mandiri extends bankProviderMixins(PaymentProvider) {
+   paymentName() {}
+   amount() {}
+   bankID() {}
+   virtualAccount() {}
+}
+
+class BNI extends bankProviderMixins(PaymentProvider) {
+   paymentName() {}
+   amount() {}
+   bankID() {}
+   virtualAccount() {}
+}
+
+class OVO extends eWalletProviderMixins(PaymentProvider) {
+   paymentName() {}
+   amount() {}
+   accountID() {}
+   walletProviderID() {}
+}
+
+const main = () => {
+   const bni = new BNI()
+   bni.bankID()
+
+}
+
+main()
